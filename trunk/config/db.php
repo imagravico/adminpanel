d45a0ad03e93fc0a9c00301a86392cc106389a6c
hoa2ng <n.khanhhoang@gmail.com>
2015-01-27 11:34:54 +0700

    initializing yii2 codes

---
+++ trunk/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=admin_yiibase',
+    'username' => 'admin_yiibase',
+    'password' => 'oXEm9oSDMg',
+    'charset' => 'utf8',
+];
